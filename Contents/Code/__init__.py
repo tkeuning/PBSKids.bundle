301a07e5d9d19a674ef14b0def5ba005710fca09
Mike <mike.d.meier@gmail.com>
2012-10-05 14:10:52 -0600

    update to v2.1

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -12,88 +12,62 @@ ICON          = "icon-default.png"
 ####################################################################################################
 def Start():
     Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, NAME, ICON, ART)
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    DirectoryItem.thumb=R(ICON)
-
-    HTTP.CacheTime = CACHE_1HOUR
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = NAME
+    DirectoryObject.thumb=R(ICON)
 
 ####################################################################################################
 def MainMenu():
-    dir = MediaContainer(viewGroup="List")
-    dir.Append(Function(DirectoryItem(ShowsList, "Shows"), categoryType = "Show"))
-    dir.Append(Function(DirectoryItem(ShowsList, "Topics"), categoryType = "Channel"))
-    return dir
+    oc = ObjectContainer()
+    oc.add(DirectoryObject(key=Callback(ShowsList, categoryType="Show", title="Shows"), title="Shows"))
+    oc.add(DirectoryObject(key=Callback(ShowsList, categoryType="Channel", title="Topics"), title="Topics"))
+    return oc
 
 ####################################################################################################
-def ShowPage(sender, title, thumb):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List")
-    #title = title.replace(' ', '%20').replace('&', '%26')  ### FORMATTING FIX
-    dir.Append(Function(DirectoryItem(VideoPage, "Full Episodes", thumb=thumb), clip='false', title=title))
-    dir.Append(Function(DirectoryItem(VideoPage, "Clips", thumb=thumb), clip='true', title=title))
+def ShowPage(title, thumb):
+    oc = ObjectContainer(title2=title)
+    
+    oc.add(DirectoryObject(key=Callback(VideoPage, clip='false', title=title), title="Full Episodes", thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+    oc.add(DirectoryObject(key=Callback(VideoPage, clip='true', title=title), title="Clips", thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
 
-    if len(dir) == 0:
-        return MessageContainer("Empty", "There aren't any items")
+    if len(oc) == 0:
+        return ObjectContainer(header="Empty", message="There aren't any items")
     else:
-        return dir
+        return oc
 
 ####################################################################################################
-def VideoPage(sender, clip, title):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList")
-    title = title.replace(' ', '%20').replace('&', '%26')  ### FORMATTING FIX
-    content = JSON.ObjectFromURL(PBS_JSON % (clip, title), cacheTime=CACHE_1DAY)
-    Log(content)
+def VideoPage(clip, title):
+    oc = ObjectContainer(title2=title)
+    show_title = title.replace(' ', '%20').replace('&', '%26')  ### FORMATTING FIX
+    content = JSON.ObjectFromURL(PBS_JSON % (clip, show_title), cacheTime=CACHE_1DAY)
     for item in content['items']:
         thumb = item['thumbnailURL']
-        Log(thumb)
         link = item['URL']
-        Log(link)
-        title = item['title']
-        Log(title)
+        video_title = item['title']
         summary = item['description']
-        Log(summary)
         duration = item['length']
-        Log(duration)
-        dir.Append(Function(VideoItem(VideoPlayer, title=title, thumb=thumb, summary=summary, duration=duration), link=link))
+        
+        if clip == 'true':
+            oc.add(VideoClipObject(url=link, title=video_title, summary=summary, duration=int(duration), thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+        else:
+            oc.add(EpisodeObject(url=link, title=video_title, show=title, summary=summary, duration=int(duration), thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
 
-    if len(dir) == 0:
-        return MessageContainer("Empty", "There aren't any items")
+    if len(oc) == 0:
+        return ObjectContainer(header="Empty", message="There aren't any items")
     else:
-        return dir
+        return oc
 
 ####################################################################################################
-def VideoPlayer(sender, link):
-    dir = MediaContainer(title2=sender.itemTitle)
-    link = link + '&format=SMIL'
-    videosmil = HTTP.Request(link).content
-    clip = videosmil.split("ref src")
-    player = clip[0]
-    clip = clip[1].split('"')
-    player = player.split("meta base")
-    player = player[1].split('"')
-    player = player[1]
-    if ".mp4" in clip[1]:
-        clip = clip[1].replace(".mp4", "")
-        clip = "mp4:" + clip
-    else:
-        clip = clip[1].replace(".flv", "")
-    return Redirect(RTMPVideoItem(player, clip))
-
-####################################################################################################
-def ShowsList(sender, categoryType):
-    dir = MediaContainer(title2=sender.itemTitle, viewGroup="List")
+def ShowsList(categoryType, title):
+    oc = ObjectContainer(title2=title)
     content = JSON.ObjectFromURL(CATEGORY_LIST % categoryType)
     for item in content['items']:
         title = item['title']
-        Log(title)
         thumb = item['thumbnailURL']
-        Log(thumb)
         if thumb != "":
             if "Channel Sample" not in title:
                 if categoryType == "Show":
-                    dir.Append(Function(DirectoryItem(ShowPage, title, thumb=thumb), title=title, thumb=thumb))
+                    oc.add(DirectoryObject(key=Callback(ShowPage, title=title, thumb=thumb), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
                 else:
-                    dir.Append(Function(DirectoryItem(VideoPage, title, thumb=thumb), clip='true', title=title))
-    return dir
+                    oc.add(DirectoryObject(key=Callback(VideoPage, title=title, clip='true'), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+    return oc
